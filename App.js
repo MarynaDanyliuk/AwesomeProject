5ab4b11f84a4fdae06f007d9d84375cc7947d0d5
Mary Danyliuk <marydanyliuk@gmail.com>
2023-02-21 20:01:33 +0200

    upd App

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!!! Who am I? MaryDan</Text>
+      <Text>Hello World!!! Who am I? Mary!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
